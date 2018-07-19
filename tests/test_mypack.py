7ee4e559a0933e97a785e061ecd917d58f3c8c70
Vineet Bansal <vineetbansal@gmail.com>
2018-07-19 11:43:11 -0400

    Added tests

---
+++ tests/test_mypack.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+from mypack import create_app
+from mypack.blueprints.web import index, hello
+
+# Testing web endpoints/blueprint requires a working 'app'
+app = create_app(debug=True)
+
+
+class APITestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testWebEndpoint(self):
+        """
+        Test a web endpoint by emulating a web request
+        """
+        with app.test_request_context('/'):
+            rv = app.preprocess_request()
+            if rv is not None:
+                response = app.make_response(rv)
+            else:
+                rv = app.dispatch_request()
+                response = app.make_response(rv)
+
+                response = app.process_response(response)
+
+        self.assertEqual(response.mimetype, 'text/html')
+        self.assertEqual(response.data.decode('utf-8'), 'Index page works!')
+
+    def testBlueprintRoute1(self):
+        """
+        Test a blueprint route directly (fast!)
+        """
+        self.assertEqual(index(), 'Index page works!')
+
+    def testBlueprintRoute2(self):
+        """
+        Test a blueprint route directly (fast!)
+        """
+        # If a blueprint renders a template, an app context needs to be pushed to the stack
+        with app.app_context():
+            self.assertEqual(hello(), 'Hello World')
